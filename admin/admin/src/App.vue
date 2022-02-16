d575f3fe6169718f9276bdcf17d7bce0beed0af1
ChristianManu <manuc@mymail.vcu.edu>
2022-02-16 18:51:24 -0500

    initial commit

---
+++ admin/admin/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+
+  components: {
+  },
+
+  data () {
+    return {
+    }
+  },
+}
+
+
+</script>
+
+<style scoped>
+  #app {
+    background-color: #76CAFC,
+  }
+
+
+</style>
---
+++ admin/admin/src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="home">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+
+  components: {
+  },
+
+  data () {
+    return {
+    }
+  },
+}
+
+
+</script>
+
+<style>
+
+
+</style>
