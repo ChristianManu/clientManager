946a7e7a2a76a501ac1bd894f4e730e1392a2c54
ChristianManu <manuc@mymail.vcu.edu>
2022-02-16 19:04:32 -0500

    restarted vue app

---
+++ admin/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+
+
+  <v-main>
+    <router-view></router-view>
+  </v-main>
+ 
+</template>
+
+<script>
+
+export default {
+    name: 'App',
+    methods: {},
+
+}
+</script>
+
+
+<style scoped>
+
+.main-bkg {
+    position: relative
+}
+
+.centered {
+    display: flex;
+    justify-content: center;
+    vertical-align: middle;
+}
+
+.bg-wallpaper{
+    background-image: url(../assets/bg-gradient.jpeg) ;
+    height: 100vh;
+    background-size: cover;
+}
+
+.center-screen {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    min-height: 100%;
+}
+
+
+</style>
--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 Vue.use(VueRouter)
 
@@ -8,11 +7,6 @@ const router = new VueRouter({
   mode: 'history',
   base: import.meta.env.BASE_URL,
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
     
   ]
 })
