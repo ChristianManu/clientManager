fce488a7d4581fba4d9ed5281284879ec2d8dc04
ChristianManu <manuc@mymail.vcu.edu>
2022-02-19 13:24:32 -0500

    started vue project over

--- admin-view/README.md
+++ admin-view/README.md
@@ -1,4 +1,4 @@
-# admin
+# admin-view
 
 This template should help get you started developing with Vue 2 in Vite.
 
--- admin-view/src/main.js
+++ admin-view/src/main.js
@@ -2,12 +2,8 @@ import Vue from 'vue'
 import VueCompositionAPI from '@vue/composition-api'
 import { createPinia, PiniaVuePlugin } from 'pinia'
 
-
-
 import App from './App.vue'
 import router from './router'
-import vuetify from './plugins/vuetify'
-import '@babel/polyfill'
 
 Vue.use(VueCompositionAPI)
 Vue.use(PiniaVuePlugin)
@@ -15,7 +11,6 @@ Vue.use(PiniaVuePlugin)
 const app = new Vue({
   router,
   pinia: createPinia(),
-  vuetify,
   render: (h) => h(App)
 })
 
